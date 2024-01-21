e3e566635b81bcd0e87de466173ef048686b4f84
Sunera321 <konarakmssb.21@uom.lk>
2024-01-21 18:35:25 +0530

    color

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="w-screen text-center h-[200px] bg-slate-400">
+      Footer
+    </div>
   )
 }
 
