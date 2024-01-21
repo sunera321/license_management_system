072a23d8ed1fd4f73724d966732dcde5a2f2727f
Sunera321 <konarakmssb.21@uom.lk>
2024-01-21 12:20:15 +0530

    logo change

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -9,20 +9,20 @@ const Navbar = () => {
   return (
     <nav className="p-4 border-b-2 border-black-300">
       <div className="container mx-auto ">
-        <div className="flex items-center justify-between ">
-          <div className="-ml-16 font-bold text-white "><img src={Logo} alt="Logo" className="w-40"/></div>
+        <div className="flex items-center justify-between -ml-16">
+          <div className="ml-20 font-bold text-white "><img src={Logo} alt="Logo" className="w-40"/></div>
           <ul className="flex space-x-4 ml-[100px] gap-10  text-black bg-white-200 font-mono" >
             <li>
               <a href="/" >Home</a>
             </li>
             <li>
-              <a href="/controlpanel" >Control Panel</a>
+              <a href="/controlpanel" >Panel</a>
             </li>
             <li>
               <a href="/about" >About</a>
             </li>
             <li>
-              <a href="/contact_us" >Contact  Us</a>
+              <a href="/contact_us" >Contact_Us</a>
             </li>
           </ul>
           <div className="flex justify-center text-black align-middle items">
