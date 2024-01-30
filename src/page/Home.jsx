c22475811d324ebc0d3d753bc6b70074785a55c0
Sunera Sudaraka Konara <110524387+sunera321@users.noreply.github.com>
2024-01-30 21:08:15 +0530

    Add Home component to src/components/page/Home/Home.jsx

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,9 +3,74 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+     <p className='pl-10 ml-10'>
+=======
+<<<<<<< HEAD
      
       Home
      
+=======
+     <p className='ml-10  pl-10'>
+>>>>>>> cc2f0c7652818ad809deaaa4c025729e20c6e240
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur
+      consequuntur corporis delectus doloribus eos, expedita facere fugit harum,
+      impedit in, ipsa itaque, libero magnam maxime mollitia nobis, nobis nulla
+      numquam officia pariatur quasi quidem quos quos repellendus repellendus
+      repellendus repellendus repellendus repellendus repellendus repellendus
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur
+      consequuntur corporis delectus doloribus eos, expedita facere fugit harum,
+      impedit in, ipsa itaque, libero magnam maxime mollitia nobis, nobis nulla
+      numquam officia pariatur quasi quidem quos quos repellendus repellendus
+      repellendus repellendus repellendus repellendus repellendus repellendus
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur
+      consequuntur corporis delectus doloribus eos, expedita facere fugit harum,
+      impedit in, ipsa itaque, libero magnam maxime mollitia nobis, nobis nulla
+      numquam officia pariatur quasi quidem quos quos repellendus repellendus
+      repellendus repellendus repellendus repellendus repellendus repellendus
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur
+      consequuntur corporis delectus doloribus eos, expedita facere fugit harum,
+      impedit in, ipsa itaque, libero magnam maxime mollitia nobis, nobis nulla
+      numquam officia pariatur quasi quidem quos quos repellendus repellendus
+      repellendus repellendus repellendus repellendus repellendus repellendus
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur
+      consequuntur corporis delectus doloribus eos, expedita facere fugit harum,
+      impedit in, ipsa itaque, libero magnam maxime mollitia nobis, nobis nulla
+      numquam officia pariatur quasi quidem quos quos repellendus repellendus
+      repellendus repellendus repellendus repellendus repellendus repellendus
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur
+      consequuntur corporis delectus doloribus eos, expedita facere fugit harum,
+      impedit in, ipsa itaque, libero magnam maxime mollitia nobis, nobis nulla
+      numquam officia pariatur quasi quidem quos quos repellendus repellendus
+      repellendus repellendus repellendus repellendus repellendus repellendus
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur
+      consequuntur corporis delectus doloribus eos, expedita facere fugit harum,
+      impedit in, ipsa itaque, libero magnam maxime mollitia nobis, nobis nulla
+      numquam officia pariatur quasi quidem quos quos repellendus repellendus
+      repellendus repellendus repellendus repellendus repellendus repellendus
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur
+      consequuntur corporis delectus doloribus eos, expedita facere fugit harum,
+      impedit in, ipsa itaque, libero magnam maxime mollitia nobis, nobis nulla
+      numquam officia pariatur quasi quidem quos quos repellendus repellendus
+      repellendus repellendus repellendus repellendus repellendus repellendus
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur
+      consequuntur corporis delectus doloribus eos, expedita facere fugit harum,
+      impedit in, ipsa itaque, libero magnam maxime mollitia nobis, nobis nulla
+      numquam officia pariatur quasi quidem quos quos repellendus repellendus
+      repellendus repellendus repellendus repellendus repellendus repellendus
+     </p>
+<<<<<<< HEAD
+
+=======
+>>>>>>> Dev-Branch
+>>>>>>> cc2f0c7652818ad809deaaa4c025729e20c6e240
+=======
+     
+      Home
+     
+>>>>>>> parent of cc2f0c7 (Add autoprefixer and postcss dependencies)
     </div>
   )
 }
