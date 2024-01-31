90eb973a8dc4984e6a7e7db64d648a20c0165591
Sunera Sudaraka Konara <110524387+sunera321@users.noreply.github.com>
2024-01-31 17:13:24 +0530

    Update sidebar icons and links

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Home from './page/Home';
 //import About from './page/About';
 import Contact_Us from './page/Contact_Us';
 import ControlPanel from './page/ControlPanel';
-
 //import KeyGenerate from './page/KeyGenerateForm';
 //import EditProfilePartner from './page/EditProfilePartner';
 //import EditProfileUser from './page/EditProfileUser';
