abe310974e723ac3802c1c8db2ed09b73254f67b
19840769 <kodikarah.21@uom.lk>
2024-02-08 07:52:56 +0530

    Update pageHeader and Slidebar components

--- src/components/CommonModal/pageHeader.jsx
+++ src/components/CommonModal/pageHeader.jsx
@@ -4,7 +4,7 @@ const PageHeader = ({ title }) => {
   return (
     <div>
       <pageHead>
-        <div className="text-[#5B5B5B] text-[30px] uppercase font-bold text-center mt-3.5 mb-3.5 font-serif">
+        <div className="text-[#5B5B5B] text-[30px] uppercase font-bold text-center my-5 mb-3.5 font-serif">
           {title}
         </div>
       </pageHead>
