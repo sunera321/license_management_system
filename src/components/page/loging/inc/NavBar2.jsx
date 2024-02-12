3e59dc5c3a28517a87521cc0298b39ca04a82765
Sunera Sudaraka Konara <110524387+sunera321@users.noreply.github.com>
2024-02-13 00:16:02 +0530

    Update logo in NavBar2 component

--- src/components/page/loging/inc/NavBar2.jsx
+++ src/components/page/loging/inc/NavBar2.jsx
@@ -5,9 +5,9 @@ function Navbar2() {
         <nav className="flex items-center justify-between h-10 px-4 bg-body-tertiary">
             {/* Add a spacer to push the logo to the right corner */}
             <div></div>
-            <a  >
+            
                 <img className='w-40 h-20 ' src={logo} alt="Hsenid" />
-            </a>
+            
         </nav>
     );
 }
