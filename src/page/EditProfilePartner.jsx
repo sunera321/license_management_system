b16db9dc7acec71de76b44443a7a4a1b4cf09216
19840769 <kodikarah.21@uom.lk>
2024-02-13 23:42:22 +0530

    Update profile link in Header component and improve styling in EditProfilePartner page

--- src/page/EditProfilePartner.jsx
+++ src/page/EditProfilePartner.jsx
@@ -8,26 +8,26 @@ const EditProfilePartner = () => {
         <div>
            <PageHeader title={'Account Settings'} />
             <div className=''>
-                <form className='w-1/3 px-8 pt-0 pb-4 mx-auto mt-10 mb-10 bg-white rounded shadow-md '>
+                <form className='w-2/5 px-10 pt-10 pb-20 mx-auto mb-10 bg-gray-100 rounded shadow-lg '>
                 <div className="flex gap-0 mb-6 ">
-                        <label className='items-start justify-start flex-1 block mb-1 text-lg font-bold text-gray-700 '>ID</label>
+                        <label className='block mx-3 mb-0 text-base font-semibold text-gray-700'>ID</label>
                         <p className='flex-1'>ID</p>
                     </div>
                     <div className="mb-6 ">
-                        <label className='block mb-1 text-lg font-bold text-gray-700 '>Name</label><br />
-                        <input type="text" required name="Name" className='w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline' /> <br />
+                        <label className='block mx-3 mb-0 text-base font-semibold text-gray-700 '>Name</label><br />
+                        <input type="text" required name="Name" className='w-full px-2 py-1 mx-2 leading-tight text-gray-700 border rounded shadow appearance-none' /> <br />
                     </div>
                     <div className='mb-6'>
-                        <label className='block mb-1 text-lg font-bold text-gray-700 '>Email</label><br />
-                        <input type="text" required name="Email" className='w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline' /><br />
+                        <label className='block mx-3 mb-0 text-base font-semibold text-gray-700 '>Email</label><br />
+                        <input type="text" required name="Email" className='w-full px-2 py-1 mx-2 leading-tight text-gray-700 border rounded shadow appearance-none' /><br />
                     </div>
                     <div className='mb-8'>
-                        <label className='block mb-1 text-lg font-bold text-gray-700 '>Phone Number</label><br />
-                        <input type="text" required name="PN" className='w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline' /> <br />
+                        <label className='block mx-3 mb-0 text-base font-semibold text-gray-700 '>Phone Number</label><br />
+                        <input type="text" required name="PN" className='w-full px-2 py-1 mx-2 leading-tight text-gray-700 border rounded shadow appearance-none' /> <br />
                     </div>
                     <div className='mb-8'>
-                        <label className='block mb-1 text-lg font-bold text-gray-700 '>Country</label><br />
-                        <input type="text" required name="PN" className='w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline' /> <br />
+                        <label className='block mx-3 mb-0 text-base font-semibold text-gray-700'>Country</label><br />
+                        <input type="text" required name="PN" className='w-full px-2 py-1 mx-2 leading-tight text-gray-700 border rounded shadow appearance-none' /> <br />
                     </div>
                     <button id='submit' type='submit' className="w-48 p-2 mt-10 font-bold text-white bg-blue-900 rounded-md shadow-xl ml-60 hover:bg-indigo-500 ">SUBMIT</button>
                 </form>
--- src/page/EditProfileUser.jsx
+++ src/page/EditProfileUser.jsx
@@ -8,32 +8,32 @@ const EditProfileUser = () => {
     return (
         <div>
          <PageHeader title={'Edit Account'} />
-            <div className='px-40 md:mx-96'>
-                <form className='px-5 pt-2 pb-20 bg-gray-100 rounded shadow-lg '>
+            <div className=''>
+                <form className='w-2/5 px-10 pt-10 pb-20 mx-auto mb-10 bg-gray-100 rounded shadow-lg'>
                 <div className="flex gap-0 mb-6 ">
-                        <label className='items-start justify-start flex-1 block mb-1 text-lg font-bold text-gray-700 '>Emp ID</label>
+                        <label className='block mx-3 mb-0 text-base font-semibold text-gray-700 '>Emp ID</label>
                         <p className='flex-1'>ID</p>
                     </div>
                     
                     <div className='mb-6'>
-                        <label className='mb-2 text-lg font-bold text-gray-700'>Name</label><br />
-                        <input type="text" name="Email" className='w-full px-3 py-2 mt-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline' /><br />
+                        <label className='block mx-3 mb-0 text-base font-semibold text-gray-700 '>Name</label><br />
+                        <input type="text" name="Email" className='w-full px-2 py-1 mx-2 leading-tight text-gray-700 border rounded shadow appearance-none ' /><br />
                     </div>
                     <div className='mb-6'>
-                        <label className='mb-3 text-lg font-bold text-gray-700'>Email</label><br />
-                        <input type="text"  name="PN" className='w-full px-3 py-2 mt-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline' /> <br />
+                        <label className='block mx-3 mb-0 text-base font-semibold text-gray-700 '>Email</label><br />
+                        <input type="text"  name="PN" className='w-full px-2 py-1 mx-2 leading-tight text-gray-700 border rounded shadow appearance-none ' /> <br />
                     </div>
                     <div className='mb-6'>
-                        <label className='mb-1 text-lg font-bold text-gray-700'>Department</label><br />
-                        <select className='w-full px-3 py-2 mt-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline' id="grid-state">
+                        <label className='block mx-3 mb-0 text-base font-semibold text-gray-700 '>Department</label><br />
+                        <select className='w-full px-2 py-1 mx-2 leading-tight text-gray-700 border rounded shadow appearance-none '>
                             <option>Finance Department</option>
                             <option>Partner Department</option>
 
                         </select>
                     </div>
                     <div className='mb-6'>
-                        <label className='mb-2 text-lg font-bold text-gray-700'>Phone Number</label><br />
-                        <input type="text" required name="PN" className='w-full px-3 py-2 mt-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline' /> <br />
+                        <label className='block mx-3 mb-0 text-base font-semibold text-gray-700 '>Phone Number</label><br />
+                        <input type="text" required name="PN" className='w-full px-2 py-1 mx-2 leading-tight text-gray-700 border rounded shadow appearance-none ' /> <br />
                     </div>
                     <div className='mb-5 '>
                         <button id='submit' type='submit' className='max-w-xl px-4 py-2 text-white bg-blue-900 rounded float-end hover:bg-blue-700'>SUBMIT</button>
@@ -44,6 +44,7 @@ const EditProfileUser = () => {
             <div className='mt-4 '>
 
             </div>
+            
         </div>
     );
 };
