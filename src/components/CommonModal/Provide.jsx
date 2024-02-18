1fe72ea8dbefb0a69b90cab2cfedadd094ba1c16
19840769 <kodikarah.21@uom.lk>
2024-02-18 06:50:31 +0530

    Update image imports and button styles

--- src/components/CommonModal/Provide.jsx
+++ src/components/CommonModal/Provide.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Provide = ({value}) => {
   return (
     <div>
-      <button className='py-2 mx-12 text-white transition duration-300 ease-in-out delay-150 bg-gray-600 rounded-full px-7 hover:-translate-y-1 hover:scale-110 '>
+      <button className='px-5 py-2 mx-12 text-white transition duration-300 ease-in-out delay-150 bg-gray-600 rounded-full '>
             
             {value}
             
