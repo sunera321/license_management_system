fc780bbde41446796598aaa53e87bbc9542c525e
19840769 <kodikarah.21@uom.lk>
2024-04-12 23:10:47 +0530

    Refactor file structure and update import paths

--- src/page/ControlPanel.jsx
+++ src/page/ControlPanel.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ClientList from '../components/page/ControlPanel/ClientList'
-import SearchBar from '../components/page/ControlPanel/searchBar'
+
 const ControlPanel = () => {
   return (
       <div>
-      <SearchBar/>
+       
       <ClientList/>
     </div>
   )
