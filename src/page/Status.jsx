78a0906c2a5821c4f1c63d0d0dac8e10458b9c18
19840769 <kodikarah.21@uom.lk>
2024-05-21 23:13:11 +0530

    chore: Update Status component to use PageLoader for loading state

--- src/page/Status.jsx
+++ src/page/Status.jsx
@@ -12,6 +12,7 @@ import BlueButton from '../components/CommonModal/BlueButton';
 import InProgress from '../components/CommonModal/InProgress';
 import Pending from '../components/CommonModal/Pending';
 import Swal from 'sweetalert2';
+import PageLoader from '../components/CommonModal/PageLoader';
 
 
 
@@ -29,7 +30,7 @@ const Status = () => {
     const [RejectRequests, setRejectRequests] = useState([]);
     const [PendingResults, setPendingResults] = useState([]);
     const [selectedDataType, setSelectedDataType] = useState('withCommentFinanceMgt'); // Default selected data type
-    const [loading, setLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     const [generatedKey, setGeneratedKey] = useState('');
     const [buttonClicked, setButtonClicked] = useState(false); // State to track if the button is clicked
     const navigate = useNavigate();
@@ -47,6 +48,7 @@ const Status = () => {
 
 
     const getData = () => {
+        setIsLoading(true);
         axios.get('https://localhost:7295/api/RequestKey')
             .then((result) => {
                 // Filter data where CommentFinaceMgt is NULL
@@ -54,9 +56,11 @@ const Status = () => {
                 const dataWithoutComment = result.data.filter(item => item.commentFinaceMgt === null);
                 setRejectRequests(dataWithComment);
                 setPendingResults(dataWithoutComment);
+                setIsLoading(false);
             })
             .catch((error) => {
                 console.log(error);
+                setIsLoading(false);
             });
     };
  
@@ -97,6 +101,9 @@ const Status = () => {
     return (
         <div>
             <PageHeader title='Approval Status' />
+            {isLoading ? (
+                <PageLoader />
+            ) : (
             <div className='mt-10 '>
                 <div className="mb-10 text-center">
                     <select className="w-1/4 p-2 border border-gray-300 rounded-md " onChange={handleSelectChange} value={selectedDataType}>
@@ -257,11 +264,12 @@ const Status = () => {
                     </tbody>
                 </table>
             </div>
-            
+            )}
             <div className='fixed top-20 right-10 '><BlueButton className="" value={"Generate Key"} href={"/keygenerate"} /> </div>
         
         </div>
     );
+
 };
 
 export default Status;
