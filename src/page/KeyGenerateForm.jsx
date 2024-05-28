be6b7e87e252774aec9f92e7b84b8bf6bf06fbc9
19840769 <kodikarah.21@uom.lk>
2024-05-28 23:26:30 +0530

    chore: Refactor module selection in KeyGenerateForm to improve readability

--- src/page/KeyGenerateForm.jsx
+++ src/page/KeyGenerateForm.jsx
@@ -180,12 +180,13 @@ const KeyGenerateForm = () => {
                         <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex ">
                             <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r">
                                 <div className="flex items-center bg-white ps-3">
-                                    <input name="Modules" id="module1-checkbox-list" onChange={(e) => handleCheckboxChange(e, "Banking, Finance & Insurance")} checked={modulesList.includes("Banking, Finance & Insurance")} type="radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 focus:ring-2" />                                    <label for="module1-checkbox-list" class="w-full py-3 ms-2 text-sm font-medium text-black">Banking, Finance & Insurance</label>
+                                    <input name="Modules" id="module1-checkbox-list" onChange={(e) => handleCheckboxChange(e, "Banking, Finance & Insurance")}  type="radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 focus:ring-2" />                                   
+                                     <label for="module1-checkbox-list" class="w-full py-3 ms-2 text-sm font-medium text-black">Banking, Finance & Insurance</label>
                                 </div>
                             </li>
                             <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r">
                                 <div className="flex items-center bg-white ps-3">
-                                    <input name="Modules" id="module2-checkbox-list" type="radio" onChange={(e) => handleCheckboxChange(e, "Manufacturing and Retail")} checked={modulesList.includes("Manufacturing and Retail")} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 " />
+                                    <input name="Modules" id="module2-checkbox-list" type="radio" onChange={(e) => handleCheckboxChange(e, "Manufacturing and Retail")}  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 " />
                                     <label for="module2-checkbox-list" class="w-full py-3 ms-2 text-sm font-medium text-black">Manufacturing and Retail</label>
                                 </div>
                             </li>
