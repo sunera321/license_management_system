cb908b04d6395d7e64b4644137c7cab981dc8d2b
dsranasinghe <110891644+dsranasinghe@users.noreply.github.com>
2024-06-16 15:00:00 +0530

    dashboard new

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -1,38 +1,48 @@
 import React, { useState } from 'react';
 import LineGraph from './../components/page/Dashboard/LineGraph';
 import BarGraph from '../components/page/Dashboard/BarGraph';
-import { lineChartData, barChartData } from '../components/page/Dashboard/Data';
 import { Link } from 'react-router-dom';
 
 
 const Dashboard = () => {
-
   const availableLicenseKeys = 7;
-  const [data] = useState({ line: lineChartData, bar: barChartData });
   const expiredLicenseKeys = 2;
   const activeLicenseKeys = 5;
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+
   return (
     <div className="mt-10 mx-6">
-      <div className="flex flex-wrap justify-between gap-4 ">
-        <div className="flex-auto min-w-[250px] max-w-full md:w-1/4 rounded-lg h-[145px] bg-blue-200 p-5 shadow-md ">
-          <span className="text-lg font-bold text-gray-600">ISSUED LICENSE KEY </span>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="rounded-lg h-[145px] bg-blue-200 p-5 shadow-md">
+          <span className="text-lg font-bold text-gray-600">ISSUED LICENSE KEY</span>
           <div className="text-4xl font-bold text-gray-600">{availableLicenseKeys}</div>
           <div className="pt-4">
             <hr className="border-t border-blue-400" />
+
             <Link to="/availablelicense" className="mt-2 text-m font-bold text-gray-500 hover:text-gray-700 flex items-center justify-center">
               View More
-            </Link>
+            </Link>           
           </div>
         </div>
-        <div className="flex-auto min-w-[250px] max-w-full md:w-1/4 rounded-lg h-[145px] bg-emerald-200 p-5 shadow-md">
-          <span className="text-lg font-bold text-gray-600">ACTIVE LICENSE KEY </span>
+        <div className="rounded-lg h-[145px] bg-emerald-200 p-5 shadow-md">
+          <span className="text-lg font-bold text-gray-600">ACTIVE LICENSE KEY</span>
           <div className="text-4xl font-bold text-gray-600">{activeLicenseKeys}</div>
           <div className="pt-4">
             <hr className="border-t border-emerald-400" />
             <Link to='/activelicense' className="mt-2 text-m font-bold text-gray-500 hover:text-gray-700 flex items-center justify-center"> View More </Link>
           </div>
         </div>
-        <div className="flex-auto min-w-[250px] max-w-full md:w-1/4 rounded-lg h-[145px] bg-violet-200 p-5 shadow-md">
+        <div className="rounded-lg h-[145px] bg-violet-200 p-5 shadow-md">
           <span className="text-lg font-bold text-gray-600">EXPIRED LICENSE KEY</span>
           <div className="text-4xl font-bold text-gray-600">{expiredLicenseKeys}</div>
           <div className="pt-4">
@@ -41,31 +51,20 @@ const Dashboard = () => {
           </div>
         </div>
       </div>
-      <div className="bg-slate-100 rounded-lg mt-10 mb-10 p-6 flex flex-col">
+
+      <div className="bg-slate-100 rounded-lg mt-10 mb-10 p-2 sm:p-6 flex flex-col">
         <h1 className='font-bold'>Total users</h1>
         <div className="flex-grow">
-          <LineGraph chartData={data.line} />
+          <LineGraph />
         </div>
       </div>
-      <div className="bg-slate-100 rounded-lg mt-10 mb-10 p-6 flex flex-col">
+      <div className="bg-slate-100 rounded-lg mt-10 mb-10 p-2 sm:p-6 flex flex-col">
         <div className="flex-grow">
-          <BarGraph chartData={data.bar} />
+          <BarGraph />
         </div>
       </div>
-
     </div>
   )
 }
 
 export default Dashboard;
-/*/* <Link to="/availablelicense">
-      <div className="flex-auto min-w-[250px] max-w-full md:w-1/4 rounded-lg h-[145px] shadow-md m-2 overflow-hidden">
-        <div className="bg-blue-300 p-5 flex items-center justify-center h-1/3">
-          <div className="text-lg font-bold text-black">ISSUED LICENSE KEY</div>
-        </div>
-        <div className="bg-sky-200 p-5 flex items-center justify-center h-2/3">
-          <div className="text-5xl font-bold text-gray-600">{availableLicenseKeys}</div>
-        </div>
-      </div>
-    </Link>
-*/
