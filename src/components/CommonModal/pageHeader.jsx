caaa43a2f721ef923a2ca40bdbf8e43ba7ab3e27
Sunera Sudaraka Konara <110524387+sunera321@users.noreply.github.com>
2024-07-01 21:19:41 +0530

    update header

--- src/components/CommonModal/pageHeader.jsx
+++ src/components/CommonModal/pageHeader.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 const PageHeader = ({ title }) => {
   return (
     <div>
-  
+      <pageHead>
         <div className="text-[#5B5B5B] text-[30px] uppercase font-bold text-center mt-3.5 mb-3.5">
           {title}
         </div>
-    
+      </pageHead>
     </div>
   );
 };
 
-<<<<<<< HEAD
-=======
 PageHeader.propTypes = {
   title: PropTypes.string.isRequired,
 };
->>>>>>> 19395d82e9b6e7af835329b3d01a383324b4b8cf
 
 export default PageHeader;
