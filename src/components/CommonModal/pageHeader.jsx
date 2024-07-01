58697aec8c6c4bb7af6c940ce7bab2fe43b89e76
Sunera Sudaraka Konara <110524387+sunera321@users.noreply.github.com>
2024-07-01 21:02:35 +0530

    bug fix

--- src/components/CommonModal/pageHeader.jsx
+++ src/components/CommonModal/pageHeader.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
 const PageHeader = ({ title }) => {
   return (
     <div>
-      <pageHead>
+  
         <div className="text-[#5B5B5B] text-[30px] uppercase font-bold text-center mt-3.5 mb-3.5">
           {title}
         </div>
-      </pageHead>
+    
     </div>
   );
 };
 
-Reject.propTypes = {
-  title: PropTypes.string.isRequired,
-};
 
 export default PageHeader;
