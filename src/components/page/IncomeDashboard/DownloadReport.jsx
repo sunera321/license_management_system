0ef63bd1a0bb5164fecefefe3a391034a1c59c3a
Sadeesha Semini <gunathilakagss.21@uom.lk>
2024-07-02 00:58:19 +0530

    newcommit

---
+++ src/components/page/IncomeDashboard/DownloadReport.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import logo from '../IncomeDashboard/hsenid.png';
+import { DownloadIcon } from '@heroicons/react/outline'; // Assuming you have imported Heroicons DownloadIcon
+
+const DownloadReport = ({ dashboardData, lineChartData, chartData, barChartData, currentDateTime }) => {
+  const downloadReport = () => {
+    const doc = new jsPDF();
+    const logoImg = new Image();
+    logoImg.src = logo;
+
+    // Calculate position based on desired margin-left and top margin
+    const imgWidth = 40; // Adjust as needed
+    const imgHeight = 40; // Adjust as needed
+    const imgX = 170; // Margin-left of 500px
+    const imgY = 10; // Margin-top of 10px
+
+    doc.addImage(logoImg, 'PNG', imgX, imgY, imgWidth, imgHeight);
+
+    doc.setFontSize(18);
+    doc.text('Income Dashboard Report', 14, 22);
+    doc.setFontSize(11);
+    doc.text(`Date: ${currentDateTime.toLocaleDateString()}`, 14, 30);
+    doc.text(`Time: ${currentDateTime.toLocaleTimeString()}`, 14, 36);
+
+    // Table 1: Dashboard Metrics
+    doc.setFontSize(14);
+    doc.text('Dashboard Metrics', 14, 50);
+    doc.autoTable({
+      startY: 55,
+      head: [['Metric', 'Value']],
+      body: [
+        ['Total Modules', dashboardData.totalModules],
+        ['Total Users', dashboardData.totalUsers],
+        ['Total Revenue', `Rs ${dashboardData.totalRevenue}`],
+      ],
+    });
+
+    // Table 2: Line Chart Data
+    doc.setFontSize(14);
+    doc.text('Company Revenue in 2023 and 2024', 14, doc.autoTable.previous.finalY + 10);
+    const lineChartTableBody = lineChartData.map(item => [
+      `${item.year}-${item.month}`, item.totalModulePrice
+    ]);
+    doc.autoTable({
+      startY: doc.autoTable.previous.finalY + 15,
+      head: [['Year-Month', 'Total Module Price']],
+      body: lineChartTableBody,
+    });
+
+    // Table 3: Chart Data
+    doc.setFontSize(14);
+    doc.text('Revenue by Module', 14, doc.autoTable.previous.finalY + 10);
+    const chartTableBody = chartData.map(item => [
+      item.moduleName, `Rs ${item.totalRevenue}`
+    ]);
+    doc.autoTable({
+      startY: doc.autoTable.previous.finalY + 15,
+      head: [['Module Name', 'Total Revenue']],
+      body: chartTableBody,
+    });
+
+    // Table 4: Bar Chart Data
+    doc.setFontSize(14);
+    doc.text('Top 5 Modules in 2024 ', 14, doc.autoTable.previous.finalY + 10);
+    const barChartTableBody = barChartData.map(item => [
+      item[0], `Rs ${item[1]}`, item[2]
+    ]);
+    doc.autoTable({
+      startY: doc.autoTable.previous.finalY + 15,
+      head: [['Module Name', 'Total Revenue', 'Number of Clients']],
+      body: barChartTableBody,
+    });
+
+    doc.save('IncomeDashboardReport.pdf');
+  };
+
+  return (
+    <div className="flex items-center justify-center">
+      <button onClick={downloadReport} className="flex items-center px-4 py-2 text-white bg-blue-600 hover:bg-blue-700 hover:text-black rounded-lg font-bold ">
+        <DownloadIcon className="w-5 h-5 mr-2" /> Download Report
+      </button>
+    </div>
+  );
+};
+
+export default DownloadReport;
--- src/page/IncomeDashboard.jsx
+++ src/page/IncomeDashboard.jsx
@@ -1,34 +1,88 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCubes, faUsers, faCoins } from '@fortawesome/free-solid-svg-icons';
-import LineChart from '../components/LineChart';
-import Chart from '../components/Chart';
-import BarChart from '../components/BarChart';
-
+import LineChart from '../components/page/IncomeDashboard/LineChart';
+import Chart from '../components/page/IncomeDashboard/Chart';
+import BarChart from '../components/page/IncomeDashboard/BarChart';
+import axiosInstance from '../components/axiosInstance';
+import DownloadReport from '../components/page/IncomeDashboard/DownloadReport';
+import DoughnutChart from '../components/page/IncomeDashboard/DonutChart';
+import axios from 'axios';
 
 function IncomeDashboard() {
   const [dashboardData, setDashboardData] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [lineChartData, setLineChartData] = useState([]);
+  const [chartData, setChartData] = useState([]);
+  const [barChartData, setBarChartData] = useState([]);
   const [currentDateTime, setCurrentDateTime] = useState(new Date());
 
   const fetchDashboardData = async () => {
     try {
-      const response = await fetch('https://localhost:7295/api/Dashboard/getdashboarddata');
+      const response = await fetch('https://localhost:7295/api/InDashboard/getdashboarddata');
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
       const data = await response.json();
-      console.log('Fetched data:', data);
       setDashboardData(data);
-      setLoading(false);
     } catch (error) {
       console.error('Error fetching data:', error);
-      setLoading(false);
+    }
+  };
+
+  const fetchLineChartData = async () => {
+    try {
+      const response = await fetch('https://localhost:7295/api/InDashboard/module-prices-by-year-month');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setLineChartData(data);
+    } catch (error) {
+      console.error('Error fetching line chart data:', error);
+    }
+  };
+
+  const fetchChartData = async () => {
+    try {
+      const response = await axios.get('https://localhost:7295/api/InDashboard/module-revenue-2024');
+      setChartData(response.data);
+    } catch (error) {
+      console.error('Error fetching chart data:', error);
+    }
+  };
+
+  const fetchCurrentMonthRevenue = () => {
+    if (lineChartData.length === 0) return null;
+
+    const currentMonth = currentDateTime.getMonth() + 1; // getMonth() returns 0-indexed month
+    const currentYear = currentDateTime.getFullYear();
+
+    const currentMonthData = lineChartData.find(
+      data => data.year === currentYear && data.month === currentMonth
+    );
+
+    return currentMonthData ? currentMonthData.totalModulePrice : 0;
+  };
+
+  const fetchBarChartData = async () => {
+    try {
+      const response = await axiosInstance.get('https://localhost:7295/api/InDashboard/top-modules');
+      const data = response.data.map(module => [
+        module.moduleName,
+        module.totalRevenue,
+        module.numberOfClients
+      ]);
+      setBarChartData(data);
+    } catch (error) {
+      console.error('Error fetching bar chart data:', error);
     }
   };
 
   useEffect(() => {
     fetchDashboardData();
+    fetchLineChartData();
+    fetchChartData();
+    fetchBarChartData();
 
     // Update current date and time every second
     const interval = setInterval(() => {
@@ -38,24 +92,20 @@ function IncomeDashboard() {
     return () => clearInterval(interval); // Cleanup interval on unmount
   }, []);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
   return (
     <div className='relative flex flex-col items-start justify-between h-full col-span-2 gap-3 p-2 ml-11'>
       {/* Current Date and Time Container */}
       <div className="absolute top-0 right-0 p-4">
-        <div className="items-center p-2 mr-10 shadow-md lg:w-54">
-          <p className="text-ash">Date:  {currentDateTime.toLocaleDateString()}</p>
-          <p className="text-ash">Time:   {currentDateTime.toLocaleTimeString()}</p>
+        <div className="items-center p-2 shadow-md lg:w-54 mr-10">
+          <p className="text-gray-600">Date: {currentDateTime.toLocaleDateString()}</p>
+          <p className="text-gray-600">Time: {currentDateTime.toLocaleTimeString()}</p>
         </div>
       </div>
 
       <div className="flex items-center justify-between w-full mb-4">
         <div>
           <h1 className="text-3xl font-bold">Dashboard</h1>
-          <p className="text-ash">Hi, Welcome back to Hsenid Admin!</p>
+          <p className="text-gray-600">Hi, Welcome back to Hsenid Admin!</p>
         </div>
       </div>
 
@@ -63,7 +113,7 @@ function IncomeDashboard() {
         {/* Total Module card */}
         <div className='flex flex-col items-center justify-center w-full gap-5 p-5 transition-transform bg-blue-200 rounded-xl'>
           <div className='flex items-center justify-between w-full'>
-            <h1 className='-mt-2 font-semibold text-black text-md'>Total Module</h1>
+            <h1 className='font-semibold text-black text-md'>Total Module</h1>
             <FontAwesomeIcon icon={faCubes} className='text-slate-600 size-12' />
           </div>
           <div className='flex items-center justify-between w-full'>
@@ -101,23 +151,61 @@ function IncomeDashboard() {
         </div>
       </div>
 
-      
-
-      {/* Larger BarChart */}
-      <div className="w-full mb-4 ml-64">
-        <div className='mb-4' style={{ width: '1300px' }}>
-          <BarChart />
-        </div>
-      </div>
-      
-      {/* LineChart and Chart */}
-      <div className="w-full mb-12">
-        <div className="mt-5 mb-32 ml-32 w-200">
+      {/* LineChart and Side Panels */}
+      <div className="flex flex-col lg:flex-row gap-4 w-full">
+        {/* Left Side: LineChart */}
+        <div className="w-full lg:w-2/3 mb-12">
           <LineChart />
         </div>
-        <div className="w-full mb-12">
-          <Chart />
+
+        {/* Right Side: Panels */}
+        <div className="flex flex-col lg:w-1/3 gap-4 mb-4">
+          {/* DownloadReport */}
+          <div className="shadow-2xl bg-white p-4 rounded-lg min-h-[200px]  lg:mt-0">
+            <p className="text-lg font-bold text-gray-800 mb-6">Download Your Earnings Report</p>
+            <DownloadReport
+              dashboardData={dashboardData}
+              lineChartData={lineChartData}
+              chartData={chartData}
+              barChartData={barChartData}
+              currentDateTime={currentDateTime}
+            />
+            <p className="text-sm text-gray-600 mt-5 ml-10">
+              Click the button above to download a pdf report of your earnings.
+            </p>
+          </div>
+
+          {/* This Month Revenue */}
+          <div className="shadow-2xl bg-slate-600 p-4 rounded-lg">
+            <div className="flex flex-col items-center justify-center h-full">
+              <p className="text-sm font-bold text-white mt-2">
+                This Month Revenue:
+              </p>
+              <p className="text-2xl font-bold text-white mt-2">
+                Rs {fetchCurrentMonthRevenue()}
+              </p>
+            </div>
+          </div>
         </div>
+      </div>
+
+      <div className='grid w-full grid-cols-1 gap-4 mb-4 lg:grid-cols-2'>
+        {/* BarChart */}
+        <div className="w-full">
+          <div className='mb-4'>
+            <BarChart />
+          </div>
+        </div>
+        {/* DoughnutChart */}
+        <div className="w-full">
+          <div className='mb-4'>
+            <DoughnutChart />
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full mb-12">
+        <Chart />
       </div>
     </div>
   );
